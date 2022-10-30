6558a2fe56d71f2c38bc2db3852b4208ace0f704
Katie Walters <katie_walters@live.co.uk>
2022-10-30 16:59:42 +0000

    added hamburger menu, moved everything out of folder so it will be ok to deploy on netlify...hopefully

--- my-portfolio/src/Components/Navbar/Navbar.js
+++
@@ -1,8 +0,0 @@
-export default function Navbar() {
-    return (
-    <nav className="navbar">
-    <a className="navlink" href="#app">Home</a>
-    <a className="navlink" href="#projects">Projects</a>
-    </nav>
-    )
-    }
--- my-portfolio/src/Routes/about.js
+++
@@ -1,9 +0,0 @@
-import Navbar from "../Components/Navbar/Navbar";
-
-export default function About() {
-    return (
-        <><Navbar /><div>
-            <h1>About</h1>
-        </div></>
-    );
-  }
