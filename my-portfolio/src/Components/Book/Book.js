ea9f227710944af72a5762ba81c76e243cdcc9bc
Katie Walters <katie_walters@live.co.uk>
2022-10-30 14:36:14 +0000

    changed book, updated css, installed sass, the page flip is working for book one, but not books two and free

--- my-portfolio/src/Components/Book/Book.js
+++ my-portfolio/src/Components/Book/Book.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../Book/book.scss";
 
 export default function Book({ book, link, text }) {
     const openInNewTab = (url) => {
@@ -7,37 +7,67 @@ export default function Book({ book, link, text }) {
     };
     return (
         <>
+           
+                <div class="cover">
+                    <div class="book">
+                        <label for="page-1" class="book__page book__page--1">
+                            <img src={book} alt="" />
+                        </label>
 
-        <div className="book-area">
-            <div className="book">
-                <div className="back"></div>
-                <div className="page6">
-                    {/* <div className="right-hand-text">{text}</div> */}
-                    <div onClick={() => openInNewTab(`${link}`)}>
-                        Go to live app
+                        <label for="page-2" class="book__page book__page--4">
+                            <div class="page__content">
+                                <h1 class="page__content-title">I</h1>
+                                <div class="page__content-blockquote">
+                                   
+                                </div>
+                                <div class="page__content-text">
+                                    {/* <p>His name was Gaal Dornick and he was just a country boy who had never seen Trantor before. That is, not in real life. He had seen it many times on the hyper-video, and occasionally in tremendous three-dimensional newscasts covering an Imperial Coronation or the opening of a Galactic Council. Even though he had lived all his life on the world of Synnax, which circled a star at the edges of the Blue Drift, he was not cut off from civilization, you see. At that time, no place in the Galaxy was. </p> */}
+
+                                    {/* <p>There were nearly twenty-five million inhabited planets in the Galaxy then, and not one but owed allegiance to the Empire whose seat was on Trantor. It was the last half-century in which that could be said. </p> */}
+                                    {/* <p>To Gaal, this trip was the undoubted climax of his young, scholarly life. He had been in space before so that the trip, as a voyage and nothing more, meant little to him. To be sure, he had traveled previously only as far as Synnax's only satellite in order to get the data on the mechanics of meteor driftage which he needed for his dissertation, but space-travel was all one whether one travelled half a million miles, or as many light years. </p> */}
+                                </div>
+                                <div class="page__number">3</div>
+                            </div>
+                        </label>
+
+                        {/* <!-- Resets the page --> */}
+                        <input type="radio" name="page" id="page-1" />
+
+                        {/* <!-- Goes to the second page --> */}
+                        <input type="radio" name="page" id="page-2" />
+                        <label class="book__page book__page--2">
+                            <div class="book__page-front">
+                                <div class="page__content">
+                                    <h1 class="page__content-book-title">
+                                        Book Reviews
+                                    </h1>
+                                    <h2 class="page__content-author">
+                                       Katie Walters
+                                    </h2>
+
+                                    
+
+                                    
+                                </div>
+                            </div>
+                            <div class="book__page-back">
+                                <div class="page__content">
+                                    <h1 class="page__content-title">
+                                        Technologies
+                                    </h1>
+                                    <table class="page__content-table">
+                                        
+                                    </table>
+
+                                    <div class="page__number">2</div>
+                                </div>
+                            </div>
+                        </label>
                     </div>
                 </div>
-                <div className="page5">
-                    <span className="flipH">TECH STACK;
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/>
-<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg" alt="materialui" width="40" height="40" />
-<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/>
-<img src="https://www.vectorlogo.zone/logos/netlify/netlify-icon.svg" width="40" height="40" alt="netlify"/></span>
-                </div>
-                <div className="page4"></div>
-                <div className="page3"></div>
-                <div className="page2"></div>
-                <div className="page1"></div>
-                <div className="front">
-                    <div className="image-container">
-                        <img
-                            src={book}
-                            alt="bookreviews"
-                            className="bookreviews"
-                        ></img>
-                    </div>
-                </div>
-            </div>
-        </div></>
+        
+            <div onClick={() => openInNewTab(`${link}`)}>Go to live app</div>
+            <br></br>
+        </>
     );
 }
--- my-portfolio/src/Components/Home/Home.js
+++ my-portfolio/src/Components/Home/Home.js
@@ -3,27 +3,25 @@ import { Icon } from '@iconify/react';
 export default function Home() {
 
     return(
-        <div classname="home">
-        <div id="title">
-        <h1>KATIE WALTERS</h1>
-        </div>
-        <br></br>
-        <p>Full stack developer based in the
-        <br></br>West Midlands</p>
-        <div className='icons'>
-        <a href="https://github.com/KatieClarinet" target="_blank" rel="noreferrer">
-        <Icon icon="akar-icons:github-fill" color="#ed2e38" width="48" height="48" />
-        </a>
-        <a href="https://twitter.com/katiecodes91" target="_blank" rel="noreferrer">
-        <Icon icon="akar-icons:twitter-fill" color="#ed2e38" width="48" height="48" />
-        </a>
-        
-        <a href="https://www.linkedin.com/in/katie-walters" target="_blank" rel="noreferrer">
-        <Icon icon="akar-icons:linkedin-box-fill" color="#ed2e38" width="48" height="48" />
+        <><div>
 
-        </a>
-        <Icon icon="fontisto:email" color="#ed2e38" width="48" height="48" />
-        </div>
-        </div>
+            <br></br>
+            <h1>KATIE WALTERS</h1>
+        </div><br></br><p>Junior developer based in the
+                <br></br>West Midlands</p><div className='icons'>
+                <a href="https://github.com/KatieClarinet" target="_blank" rel="noreferrer">
+                    <Icon icon="akar-icons:github-fill" color="#ed2e38" width="48" height="48" />
+                </a>
+                <a href="https://twitter.com/katiecodes91" target="_blank" rel="noreferrer">
+                    <Icon icon="akar-icons:twitter-fill" color="#ed2e38" width="48" height="48" />
+                </a>
+
+                <a href="https://www.linkedin.com/in/katie-walters" target="_blank" rel="noreferrer">
+                    <Icon icon="akar-icons:linkedin-box-fill" color="#ed2e38" width="48" height="48" />
+
+                </a>
+                <Icon icon="fontisto:email" color="#ed2e38" width="48" height="48" />
+
+            </div></>
     )
 }
