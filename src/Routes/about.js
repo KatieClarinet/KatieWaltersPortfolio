7fcbd5ae2a1d0e08fd9f2e045a9127ad63c2b16b
Katie Walters <katie_walters@live.co.uk>
2022-10-31 15:50:26 +0000

    pages now turn for each book independently, about section added, more css updates and changes

--- src/Routes/about.js
+++ src/Routes/about.js
@@ -1,9 +1,21 @@
-import Navbar from "../Components/Navbar/Navbar";
+import Katie from '../Assets/KatieWalters.png'
 
 export default function About() {
     return (
-        <><Navbar /><div>
-            <h1>About</h1>
+        <>
+        <div id="about">
+            <h1>ABOUT</h1>
+            <div className="left-right">
+            <div className="about-left">
+            <img src={Katie} alt={Katie} className='photo'/></div>
+            <div className="about-right">
+            <p>Hello, I’m Katie!
+I’m soon to graduate from the School of Code as a Junior Fullstack Developer. 
+
+
+Previously I worked in music education as a clarinet teacher and in various administrative roles.
+            </p></div>
+            </div>
         </div></>
     );
   }
