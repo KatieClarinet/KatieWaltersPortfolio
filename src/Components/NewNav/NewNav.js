f3192c3c32a2f6ec498c620dc8000a4b0d6b666e
Katie Walters <katie_walters@live.co.uk>
2022-11-01 14:04:19 +0000

    updated broken link

--- src/Components/NewNav/NewNav.js
+++ src/Components/NewNav/NewNav.js
@@ -35,7 +35,7 @@ const NewNav = () => {
         }>
         <ul>
           <li>
-            <a href="/">Home</a>   
+            <a href="/">Home</a>    
           </li>
           <li>
             <a href="#about">About</a>
