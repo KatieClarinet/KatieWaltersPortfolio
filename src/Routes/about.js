43499a89398d8a2fa7e955ab88bfeb4af47462a6
Katie Walters <katie_walters@live.co.uk>
2022-11-01 13:03:30 +0000

    updated bio

--- src/Routes/about.js
+++ src/Routes/about.js
@@ -5,23 +5,27 @@ export default function About() {
         <>
             <div id="about">
                 <h1>ABOUT</h1>
+                    <br></br>
                 <div className="left-right">
                     <div className="about-left">
                         <img src={Katie} alt={Katie} className="photo" />
                     </div>
+                        <br></br>
                     <div className="about-right">
+                    <div className='bio'>
+
                         <br></br>
-                        Hello, I’m Katie!
-                        <br></br>
-                        <br></br>A former music teacher and administrator, I am
-                        now looking for my first role as a full stack developer!
-                        <br></br>
-                        <br></br>
-                        When I'm not coding (quite rare!), I'm either reading, napping, or
-                        playing with my kitten.
+                        Originally a classically trained clarinetist, I  recently made the switch from concerts => coding!
                         <br></br>
                         <br></br>
-                        Read on to see my recent projects...
+                        My journey began with the 16 week School of Code bootcamp, where I trained in industry best practices, agile methodology, pair programming and computational thinking.                        <br></br>
+                        <br></br>
+                        I’m an avid reader (historical fiction or anything where I can get lost in other lands/cultures), collector of plants and ‘cat mom’ to my ginger kitten Crunchie!
+                        <br></br>
+                        <br></br>
+                        Based in the West Midlands, I am currently looking for my first role as a Junior Developer.
+                        <br></br>  
+                                          </div>
                     </div>
                 </div>
             </div>
