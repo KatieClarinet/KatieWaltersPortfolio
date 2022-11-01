45e50a8b951829dbf9857c7fc4a8fa70ab812c51
Katie Walters <katie_walters@live.co.uk>
2022-11-01 11:47:49 +0000

    updated styling for book content, added in the icons for tech stack (no longer hard coded), more css tweaks for responsiveness, moved info into data file

---
+++ src/Data/info.js
@@ -0,0 +1,75 @@
+import bookreviews from '../Assets/becks_bookcover.png'
+import eviechargers from "../Assets/evie_bookcover.png";
+import scalespractice from "../Assets/scales_bookcover.png";
+
+
+let info = [
+    {
+        image: bookreviews,
+        title: "Book Reviews",
+        text: `
+
+    I'm an avid reader, and also like to write book
+    reviews.
+
+    I created this simple app where users can
+    search and view my book reviews.
+    `,
+    stack: [
+        {   html: 'akar-icons:html-fill',
+            javascript: 'akar-icons:javascript-fill',
+            css: 'akar-icons:css-fill',
+            react: 'akar-icons:react-fill',
+            express: 'logos:express',
+            node: 'fa6-brands:node',
+            postgres: 'akar-icons:postgresql-fill',
+            postman: 'simple-icons:postman',
+            heroku: 'simple-icons:heroku',
+            netlify: 'simple-icons:netlify'
+        }
+    ]
+    },
+    {
+        image: eviechargers,
+        title: "EVie Chargers",
+        text: `
+
+    Some words about EVie charger project
+    `,
+    stack: [
+        {   html: 'akar-icons:html-fill',
+            javascript: 'akar-icons:javascript-fill',
+            css: 'akar-icons:css-fill',
+            react: 'akar-icons:react-fill',
+            express: 'logos:express',
+            node: 'fa6-brands:node',
+            postgres: 'akar-icons:postgresql-fill',
+            postman: 'simple-icons:postman',
+            heroku: 'simple-icons:heroku',
+            netlify: 'simple-icons:netlify'
+        }
+    ]
+    },
+    {
+        image: scalespractice,
+        title: "Scales Practice",
+        text: `
+
+Some words about Scales project
+`,
+stack: [
+    {   html: 'akar-icons:html-fill',
+        javascript: 'akar-icons:javascript-fill',
+        mui: 'simple-icons:mui',
+        react: 'akar-icons:react-fill',
+        express: 'logos:express',
+        node: 'fa6-brands:node',
+        postman: 'simple-icons:postman',
+        mongodb: 'simple-icons:mongodb',
+        netlify: 'simple-icons:netlify'
+    }
+]
+    },
+];
+
+export default info
