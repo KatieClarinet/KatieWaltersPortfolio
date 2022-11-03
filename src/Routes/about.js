ef8b015990b30aa4a468c8451df5c4da5eed338d
Katie Walters <katie_walters@live.co.uk>
2022-11-03 12:27:30 +0000

    updated css media queries, still need to do more on the navbar though

--- src/Routes/about.js
+++ src/Routes/about.js
@@ -6,17 +6,15 @@ export default function About({darkmode}) {
             <div id="about">
                 <h1>ABOUT</h1>
                     <br></br>
-                <div className="left-right">
+                <div className={ darkmode? "left-right-dark" : "left-right"}>
                     <div className="about-left">
                         <img src={Katie} alt={Katie} className="photo" />
                     </div>
                         <br></br>
                     <div className="about-right">
-                    <div className={darkmode ? "dark-box" : "pink-box"}>
 
-                    <div className='bio'>
 
-                        <br></br>
+                      
                         Originally a classically trained clarinetist, I  recently made the switch from concerts => coding!
                         <br></br>
                         <br></br>
@@ -27,8 +25,7 @@ export default function About({darkmode}) {
                         <br></br>
                         Based in the West Midlands, I am currently looking for my first role as a Junior Developer.
                         <br></br>  
-                                          </div>
-                    </div>
+
                     </div>
                 </div>
             </div>
