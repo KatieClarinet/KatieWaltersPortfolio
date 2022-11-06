5b8298c7bdbc98c9c177a8ef8712ec56c9dd498b
Katie Walters <katie_walters@live.co.uk>
2022-11-06 13:28:48 +0000

    updated styling on about me section, added border and shadow to image, made background transparent, changed text size to vh, added the flowers

--- src/Routes/about.js
+++ src/Routes/about.js
@@ -1,4 +1,6 @@
 import Katie from "../Assets/KatieWalters.png";
+import redFlower from '../Assets/redflower.png'
+import blueFlower from '../Assets/blueflower.png'
 
 export default function About({darkmode}) {
     return (
@@ -7,7 +9,7 @@ export default function About({darkmode}) {
                 <h1>ABOUT</h1>
                 <div className={ darkmode? "left-right-dark" : "left-right"}>
                     <div className="about-left">
-                        <img src={Katie} alt={Katie} className="photo" />
+                        <img src={Katie} alt={Katie} className={ darkmode? "photo-dark" : "photo" }/>
                     </div>
                         
                     <div className="about-right">
@@ -16,14 +18,16 @@ export default function About({darkmode}) {
 
 
 <br></br>
-                        Originally a classically trained clarinetist, I  recently made the switch from concerts => coding! 
+                        Originally a classically trained clarinetist,
                         <br></br>
-                        <br></br>
+                        I recently made the switch from concerts => coding! 
+                        
+                        <img src={ darkmode? blueFlower : redFlower} alt="flower" className="about-flower"/> 
+                        
                         My journey began with the 16 week School of Code bootcamp, where I trained in industry best practices, agile methodology, pair programming and computational thinking.                        <br></br>
-                        <br></br>
+                        <img src={ darkmode? blueFlower : redFlower} alt="flower" className="about-flower"/> 
                         I’m an avid reader (historical fiction or anything where I can get lost in other lands/cultures), collector of plants and ‘cat mom’ to my ginger kitten Crunchie!
-                        <br></br>
-                        <br></br>
+                        <img src={ darkmode? blueFlower : redFlower} alt="flower" className="about-flower"/> 
                         Based in the West Midlands, I am currently looking for my first role as a Junior Developer.
                         <br></br>   
  {/* </text> */}
